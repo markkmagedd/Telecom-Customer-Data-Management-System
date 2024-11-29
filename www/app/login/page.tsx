d04a0e13584f3a127e969f5bd8095c9ccf25db2d
daddylunix <ramezjoseph8@gmail.com>
2024-11-29 19:42:59 +0200

    tabs, login, customer & admin

--- www/app/login/page.tsx
+++ www/app/login/page.tsx
@@ -1,6 +1,10 @@
-export default function Page() {
+"use server"
+import { Login } from "@/components/login"
+
+export default async function Page() {
     return (
-        <>
-        </>
+        <main className="flex items-center justify-center p-12">
+        <Login />
+        </main>
     )
 }
