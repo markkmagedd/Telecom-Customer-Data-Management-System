c88d0b83964885098d12cc6b31c713069c908f14
daddylunix <ramezjoseph8@gmail.com>
2024-12-02 21:30:33 +0200

    consumption, offered plans

--- www/app/customer/[mobileNo]/consumption/page.tsx
+++ www/app/customer/[mobileNo]/consumption/page.tsx
@@ -3,7 +3,10 @@ import { ViewAll } from "@/components/view-all-user";
 export default async function Page() {
     return (
         <main className="p-6">
+        <h1 className="font-bold text-3xl">View Total Consumption from certain date</h1>
+        <div className="mt-4">
         <ViewAll />
+        </div>
         </main>
     )
 }
--- www/app/customer/[mobileNo]/layout.tsx
+++ www/app/customer/[mobileNo]/layout.tsx
@@ -38,7 +38,12 @@ export default function LayoutNavigation({ children }: { children: React.ReactNo
                   </div>
                   Total Consumption
                 </Link>
-                
+                <Link href={`/customer/${url}/offered-plans`} className={`${path.includes('/profiles') ? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
+                <div className="border border-foreground/30 rounded-md dark:bg-black p-1 bg-white">
+                  <Users className="h-4 w-4" />
+                  </div>
+                  Offered Plans
+                </Link>
               </nav>
             </div>
             <div className="p-4 w-full flex gap-2">
