2ddc2a97de0968e2b678431e6007cf3da4830d46
daddylunix <ramezjoseph8@gmail.com>
2024-12-02 19:19:30 +0200

    padding to consumption, push

--- www/app/customer/[mobileNo]/consumption/page.tsx
+++ www/app/customer/[mobileNo]/consumption/page.tsx
@@ -2,8 +2,8 @@ import { ViewAll } from "@/components/view-all-user";
 
 export default async function Page() {
     return (
-        <>
+        <main className="p-6">
         <ViewAll />
-        </>
+        </main>
     )
 }
--- www/app/customer/[mobileNo]/layout.tsx
+++ www/app/customer/[mobileNo]/layout.tsx
@@ -32,7 +32,7 @@ export default function LayoutNavigation({ children }: { children: React.ReactNo
                  </div>
                   Overview
                 </Link>
-                <Link href="/" className={`${path.includes('/profiles') ? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
+                <Link href={`/customer/${url}/consumption`} className={`${path.includes('/profiles') ? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
                 <div className="border border-foreground/30 rounded-md dark:bg-black p-1 bg-white">
                   <Users className="h-4 w-4" />
                   </div>
