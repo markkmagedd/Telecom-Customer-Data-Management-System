688afb1badd53b69e487573c1e2b5e46c7cd3aed
daddylunix <ramezjoseph8@gmail.com>
2024-12-02 18:36:56 +0200

    working on customer dashboard

--- www/app/customer/[mobileNo]/page.tsx
+++ www/app/customer/[mobileNo]/page.tsx
@@ -2,8 +2,8 @@
 
 export default async function Page({ params } : { params: { mobileNo:string } }) {
     return (
-        <>
-        {params.mobileNo}
-        </>
+        <main className="p-6">
+        <h1 className="font-bold text-3xl">Hello, {params.mobileNo} 👋🏼</h1>
+        </main>
     )
 }
